778dfac28bdc2c83f9972e1825cc3d36a62dd921
LuongPhucKhangHIIE <luongphuckhang200300hiie@gmail.com>
2020-10-02 00:09:26 +0700

    first commit

---
+++ 01_Exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Input name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+
+        }
+    }
+}
